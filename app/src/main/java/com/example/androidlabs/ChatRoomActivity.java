2699ad6ea5ed3a283949fa19d32fb658c723f18c
dennywai <denisechwai@gmail.com>
2021-11-26 16:20:13 -0500

    Lab4 Commit

--- app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
@@ -2,21 +2,17 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 
 
