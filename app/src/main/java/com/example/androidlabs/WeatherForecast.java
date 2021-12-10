b1e37b770432a55b0fa96b4be2b125299723c104
dennywai <denisechwai@gmail.com>
2021-12-09 23:38:57 -0500

    Lab6 Commit

--- app/src/main/java/com/example/androidlabs/WeatherForecast.java
+++ app/src/main/java/com/example/androidlabs/WeatherForecast.java
@@ -2,12 +2,21 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class WeatherForecast extends AppCompatActivity {
 
+    ImageView currentweather;
+    TextView currenttemp;
+    TextView mintemp;
+    TextView maxtemp;
+    TextView UV;
     ProgressBar progressbar;
 
     @Override
@@ -15,6 +24,42 @@ public class WeatherForecast extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_forecast);
 
+        currentweather = findViewById(R.id.currentweather);
+        currenttemp = findViewById(R.id.currenttemp);
+        mintemp = findViewById(R.id.mintemp);
+        maxtemp = findViewById(R.id.maxtemp);
+        UV = findViewById(R.id.UV);
+
         progressbar.setVisibility(View.VISIBLE);
+    }
+
+    private class ForecastQuery extends AsyncTask<String, Integer, String> {
+        String uv;
+        String min;
+        String max;
+        String currentTemp;
+        Bitmap weather;
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... args) {
+            progressbar.setVisibility(View.VISIBLE);
+            progressbar.setProgress(args[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            currenttemp.setText("Current Temperature is: " + currentTemp);
+            mintemp.setText("Min Temperature is: " + min);
+            maxtemp.setText("Max Temperature is: " + max);
+            UV.setText("UV Rating is: " + uv);
+            currentweather.setImageBitmap(weather);
+
+            progressbar.setVisibility(View.INVISIBLE);
+        }
     }
 }
