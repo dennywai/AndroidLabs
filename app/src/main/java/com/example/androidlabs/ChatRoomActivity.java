8f03b8365f5be59e25bbd61b6bea62a42a6abe47
dennywai <denisechwai@gmail.com>
2021-12-11 19:19:09 -0500

    Lab5 Commit

--- app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
+++ app/src/main/java/com/example/androidlabs/ChatRoomActivity.java
@@ -2,7 +2,6 @@ package com.example.androidlabs;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -18,19 +17,21 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 
 public class ChatRoomActivity extends AppCompatActivity {
 
-    private static final String ACTIVITY_NAME = "ChatRoomActivity";
+    public String ACTIVITY_NAME = "ProfileActivity";
     Button send;
     Button receive;
+    TextView sendtext;
+    TextView receivetext;
     EditText chattext;
     ArrayList<Messages> messageList = new ArrayList<>();
     Messages message;
     SQLiteDatabase db;
-    MyOpener dbOpener = new MyOpener(this);
+    Cursor cursor;
+    MyOpener opener = new MyOpener(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,16 +42,18 @@ public class ChatRoomActivity extends AppCompatActivity {
         send = findViewById(R.id.send);
         receive = findViewById(R.id.receive);
         chattext = findViewById(R.id.chattext);
+        sendtext = findViewById(R.id.sendtext);
+        receivetext = findViewById(R.id.receivetext);
 
         ListView list = findViewById(R.id.listview);
         ChatAdapter adapter = new ChatAdapter();
         list.setAdapter(adapter);
 
+        MyOpener dbOpener = new MyOpener(this);
         db = new MyOpener(this).getWritableDatabase();
-        String [] columns = {MyOpener.COL_ID, MyOpener.COL_TYPE, MyOpener.COL_MESSAGE};
-        Cursor cursor = db.query(MyOpener.TABLE_NAME, columns, null, null, null, null, null, null);
+        String [] Columns = {MyOpener.COL_ID, MyOpener.COL_TYPE, MyOpener.COL_MESSAGE};
+        cursor = db.query(false, MyOpener.TABLE_NAME, Columns, null, null, null, null, null, null);
 
-        cursor.moveToFirst();
         //find the column indices:
         int textIndex = cursor.getColumnIndex(dbOpener.COL_TYPE);
         int messageIndex = cursor.getColumnIndex(dbOpener.COL_MESSAGE);
@@ -58,7 +61,8 @@ public class ChatRoomActivity extends AppCompatActivity {
 
         //iterate over the results, return true if there is a next item:
 
-        while (!cursor.isAfterLast()) {
+        while (cursor.moveToNext()){
+                //!cursor.isAfterLast()) {
             String text = cursor.getString(textIndex);
             String message = cursor.getString(messageIndex);
             long id = cursor.getLong(idIndex);
@@ -68,7 +72,6 @@ public class ChatRoomActivity extends AppCompatActivity {
                 messageList.add(new Messages(message, false, id));
             } else if (text.equals("true")) {
                 messageList.add(new Messages(message, true, id));
-                cursor.moveToNext();
             }
         }
 
@@ -106,10 +109,11 @@ public class ChatRoomActivity extends AppCompatActivity {
             builder.setTitle("Do you want to delete this?");
             builder.setMessage("The selected row is: " + pos + " The database id is: " + id)
                     .setPositiveButton("YES", (click, arg) -> {
+                        MyOpener opener = null;
                         messageList.remove(pos);
-                        Log.e(ACTIVITY_NAME, "Message Deleted: " + pos + "Database ID: " + id + message);
+                        Log.e(ACTIVITY_NAME, "Message Deleted: " + pos + "Database ID: " + id);
                         adapter.notifyDataSetChanged();
-                        messageList.remove(db);
+                        //messageList.remove(db);
                         Toast.makeText(ChatRoomActivity.this, "Message Deleted", Toast.LENGTH_LONG).show();
                     })
                     .setNegativeButton("NO", (click, arg) -> {
@@ -117,14 +121,13 @@ public class ChatRoomActivity extends AppCompatActivity {
                     .create().show();
             return true;
         });
-
         printCursor(cursor, db.getVersion());
     }
     private void printCursor(Cursor c, int version) {
-        Log.d("Version No.: ", String.valueOf(version));
-        Log.d("Number of Columns: ", String.valueOf(c.getColumnCount()));
-        Log.d("Names of Columns: ", Arrays.toString(c.getColumnNames()));
-        Log.d("Number of Results: ", String.valueOf(c.getCount()));
+        Log.d("PrintCursor", "Version No.: " + MyOpener.VERSION_NUM);
+        Log.d("PrintCursor", "Number of Columns: " + c.getColumnCount());
+        Log.d("PrintCursor", "Names of Columns: " + c.getColumnNames());
+        Log.d("PrintCursor", "Number of Results: " + c.getCount());
 
         int textIndex = c.getColumnIndex(MyOpener.COL_TYPE);
         int messageIndex = c.getColumnIndex(MyOpener.COL_MESSAGE);
@@ -135,6 +138,7 @@ public class ChatRoomActivity extends AppCompatActivity {
             String message = c.getString(messageIndex);
             long id = c.getLong(idIndex);
             Log.d("ROW", "text: " + text + ", message: " + message + ", id:" + id);
+            c.moveToNext();
         }
     }
         protected class ChatAdapter extends BaseAdapter {
@@ -161,13 +165,12 @@ public class ChatRoomActivity extends AppCompatActivity {
                 TextView row;
                 message = (Messages) getItem(position);
 
-                if(message.isSent) {
+                if (message.isSent) {
                     view = inflater.inflate(R.layout.activity_send, parent, false);
                     row = view.findViewById(R.id.sendtext);
                     String show = message.getMessage();
                     row.setText(show);
-                }
-                else {
+                } else {
                     view = inflater.inflate(R.layout.activity_receieve, parent, false);
                     row = view.findViewById(R.id.receivetext);
                     String show = message.getMessage();
@@ -175,6 +178,9 @@ public class ChatRoomActivity extends AppCompatActivity {
                 }
                 return view;
             }
+            public void deleteFromDatabase(SQLiteDatabase db){
+                db.delete(MyOpener.TABLE_NAME, MyOpener.COL_ID + "=?", new String[]{Long.toString(message.getId())});
+            }
         }
-    }
+}
 
