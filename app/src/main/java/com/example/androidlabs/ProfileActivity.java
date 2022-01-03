f8fe1191a6288d8919d89bb5c495416b4901d89f
dennywai <denisechwai@gmail.com>
2022-01-03 13:01:20 -0500

    Lab_8 Commit

--- app/src/main/java/com/example/androidlabs/ProfileActivity.java
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -13,6 +14,7 @@ import android.widget.ImageButton;
 public class ProfileActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int TEST_TOOLBAR = 2;
     private static final String ACTIVITY_NAME = "ProfileActivity";
     ImageButton mImageButton;
 
@@ -20,10 +22,10 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        Log.e(ACTIVITY_NAME, "In onCreate");
+        Log.e(ACTIVITY_NAME, "In function: onCreate");
 
         mImageButton = (ImageButton) findViewById(R.id.picture);
-        mImageButton.setOnClickListener(v -> dispatchTakePictureIntent());
+        mImageButton.setOnClickListener(click -> dispatchTakePictureIntent());
 
         Button chatroom = findViewById(R.id.chatroom);
         Intent ChatRoomActivity1 = new Intent(this, ChatRoomActivity1.class);
@@ -32,11 +34,15 @@ public class ProfileActivity extends AppCompatActivity {
         Button weatherforecast = findViewById(R.id.weatherforecast);
         Intent WeatherForecast = new Intent(this, WeatherForecast.class);
         weatherforecast.setOnClickListener(click -> startActivity(WeatherForecast));
+
+        Button toolbar = findViewById(R.id.toolbar_button);
+        Intent testTB = new Intent(this, TestToolbar.class);
+        toolbar.setOnClickListener(click -> startActivityForResult(testTB, TEST_TOOLBAR));
     }
     @Override
     protected void onStart() {
         super.onStart();
-        Log.e(ACTIVITY_NAME, "In onStart");
+        Log.e(ACTIVITY_NAME, "In function: onStart");
     }
 
     private void dispatchTakePictureIntent() {
@@ -54,28 +60,32 @@ public class ProfileActivity extends AppCompatActivity {
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             mImageButton.setImageBitmap(imageBitmap);
         }
+        if (requestCode == TEST_TOOLBAR && resultCode == 500) {
+            finish();
+        }
+        Log.e(ACTIVITY_NAME, "In function: onActivityResult()");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.e(ACTIVITY_NAME, "In onResume");
+        Log.e(ACTIVITY_NAME, "In function: onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.e(ACTIVITY_NAME, "In onPause");
+        Log.e(ACTIVITY_NAME, "In function: onPause");
     }
     @Override
     protected void onStop() {
         super.onStop();
-        Log.e(ACTIVITY_NAME, "In onStop");
+        Log.e(ACTIVITY_NAME, "In function: onStop");
 
     } @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.e(ACTIVITY_NAME, "In onDestroy");
+        Log.e(ACTIVITY_NAME, "In function: onDestroy");
     }
 
 }
---
+++ app/src/main/java/com/example/androidlabs/TestToolbar.java
@@ -0,0 +1,89 @@
+package com.example.androidlabs;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class TestToolbar extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_toolbar);
+
+        Toolbar tBar = findViewById(R.id.toolbar);
+        setSupportActionBar(tBar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, tBar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            String message = null;
+            //Look at your menu XML file. Put a case for every id in that file:
+            switch(item.getItemId())
+            {
+                //what to do when the menu item is selected:
+                case R.id.item1:
+                    message = "You clicked on Item 1";
+                    break;
+                case R.id.item2:
+                    message = "You clicked on Item 2";
+                    break;
+                case R.id.item3:
+                    message = "You clicked on Item 3";
+                    break;
+                case R.id.item4:
+                    message = "You clicked on the Overflow Menu";
+                    break;
+            }
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        Intent navigate;
+        switch(item.getItemId())
+        {
+            case R.id.gotoChat:
+                navigate = new Intent(this, ChatRoomActivity1.class);
+                startActivity(navigate);
+                break;
+            case R.id.gotoWeather:
+                navigate = new Intent(this, WeatherForecast.class);
+                startActivity(navigate);
+                break;
+            case R.id.gotoLogin:
+                setResult(500);
+                finish();
+                break;
+        }
+        return true;
+    }
+
+}
